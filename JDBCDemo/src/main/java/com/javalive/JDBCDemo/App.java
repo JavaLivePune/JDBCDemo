c0fe8082390356e95b023810521ed7d037c1bd18
abc <abc@xyz.com>
2021-10-18 20:16:02 +0530

    Initial commit.

---
+++ JDBCDemo/src/main/java/com/javalive/JDBCDemo/App.java
@@ -0,0 +1,14 @@
+package com.javalive.JDBCDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Line from client side.");
+    }
+}
